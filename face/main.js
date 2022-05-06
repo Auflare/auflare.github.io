dbd13ae26f89d8d9ef2645f8f396de4f41e74bed
Auflare <86540724+Auflare@users.noreply.github.com>
2022-05-07 00:16:30 +0930

    Update main.js

--- face/main.js
+++ face/main.js
@@ -5,11 +5,11 @@ let ageRange = [];
 let predictions = []
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-    faceapi.nets.ageGenderNet.loadFromUri("/models")
+    faceapi.nets.tinyFaceDetector.loadFromUri("face/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("face/models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("face/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("face/models"),
+    faceapi.nets.ageGenderNet.loadFromUri("face/models")
 ]).then(init);
 
 function init() {
